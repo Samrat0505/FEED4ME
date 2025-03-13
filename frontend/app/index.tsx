ec12a30eb295f1ae506916947160d52aae481e3f
Akshay <ak74ay@gmail.com>
2025-03-13 14:41:14 +0530

    feat : added socket for messaging

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -7,8 +7,22 @@ import { Text } from "~/components/ui/text";
 import { router } from "expo-router";
 import LucidIcons from "~/components/LucidIcons";
 import { LayoutDashboardIcon, ScreenShare, Sprout } from "lucide-react-native";
+import { socket } from '../socket';
+import { useEffect, useState } from 'react';
+
 
 export default function Screen() {
+  useEffect(() => {
+    socket.on('connect', ()=>{
+      console.log('new user connected :', socket.id);
+    })
+
+    socket.on('testing', (data)=>{
+      console.log(data)
+    }) 
+  }, []);
+
+
   return (
     <View className="flex-1 justify-center items-center gap-5 p-6 bg-secondary/30">
 
